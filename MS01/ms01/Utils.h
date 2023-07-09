0f0bc4e2dd2c50a5a00961b3b75ce7949192e57d
n106ky <n106ky@gmail.com>
2023-07-09 14:12:30 -0400

    MS01

---
+++ MS01/ms01/Utils.h
@@ -0,0 +1,23 @@
+/***********************************************************************
+// OOP244 Utils Module
+// File	Utils.h
+// Version 
+// Date	
+// Author	
+// Description
+//
+// Revision History
+// -----------------------------------------------------------
+// Name            Date            Reason
+// 
+/////////////////////////////////////////////////////////////////
+***********************************************************************/
+#ifndef SDDS_UTILS_H__
+#define SDDS_UTILS_H__
+
+namespace sdds {
+	void strCpy(char* des, const char* src);
+	int strLen(const char* s);
+	void strCat(char* des, const char* src);
+}
+#endif // SDDS_UTILS_H__
